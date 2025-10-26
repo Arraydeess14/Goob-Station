8d01fc2033acd5cba135f691b8866e17ce2ea4c0
Arraydeess <arraygitbash@gmail.com>
2025-10-25 20:47:00 -0500

    Added code to system and component

--- Content.Goobstation.Server/Bloons/BloonSpawnerSystem.cs
+++ Content.Goobstation.Server/Bloons/BloonSpawnerSystem.cs
@@ -1,6 +1,33 @@
+using Content.Goobstation.Common.Chat;
+using Content.Goobstation.Common.Traits;
+using Content.Goobstation.Shared.Bloons;
+using Content.Server.Power.Components;
+using Content.Server.Radio;
+using Content.Shared.Interaction;
+using Content.Shared.Interaction.Events;
+using Robust.Shared.GameStates;
+using System.Threading.Tasks;
+
 namespace Content.Goobstation.Server.Bloons;
 
 public sealed class BloonSpawnerSystem : EntitySystem
 {
 
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<BloonSpawnerComponent, ActivateInWorldEvent>(OnBloonSpawnerUse);
+    }
+
+    private void OnBloonSpawnerUse(EntityUid uid, BloonSpawnerComponent component, ref ActivateInWorldEvent args)
+    {
+        if (args.Handled)
+            return;
+        var coords = Transform(uid).Coordinates;
+
+        foreach(string bloon in component.BloonID)
+        {
+            Spawn(bloon, coords);
+        }
+    }
 };
+
--- Content.Goobstation.Shared/Bloons/BloonSpawnerComponent.cs
+++ Content.Goobstation.Shared/Bloons/BloonSpawnerComponent.cs
@@ -1,7 +1,12 @@
 namespace Content.Goobstation.Shared.Bloons;
 
 [RegisterComponent]
-public sealed class BloonSpawnerComponent : Component
+public sealed partial class BloonSpawnerComponent : Component
 {
+    [DataField("bloonId", required: true)]
+    public string[] BloonID = new string[10];
+
+    [DataField("delay", required: true)]
+    public int Delay = 500;
 
 };
